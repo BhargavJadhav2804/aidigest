66b7f72a34dd54be6dbee6d33e4abfc6b52b105b
BhargavJ <141632431+BhargavJadhav2804@users.noreply.github.com>
2025-01-25 08:05:58 +0000

    25-1-25try more

--- app/src/lib/server/db/index.ts
+++ app/src/lib/server/db/index.ts
@@ -1,6 +1,7 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { DATABASE_URL } from '$env/static/private';
+import { drizzle } from 'drizzle-orm/postgres-js'
+// import { DATABASE_URL } from '$env/static/private';
 import * as schema from "./schema"
-if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+import { DATABASE_URL } from '$env/static/private';
+//if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export const db = drizzle(DATABASE_URL,{schema});
