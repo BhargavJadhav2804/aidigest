dce2150a79c0220421591b77bc69f4bc6e7ac91c
BhargavJ <141632431+BhargavJadhav2804@users.noreply.github.com>
2025-01-27 17:56:59 +0000

    add analytics

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -7,7 +7,10 @@
 	import NAV from '$lib/components/Nav.svelte';
 	import { bounceInOut, cubicInOut } from 'svelte/easing';
 	import { theme } from '$lib/utils.svelte';
-	
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+	injectAnalytics();
+
 	let { children } = $props();
 </script>
 
@@ -17,7 +20,7 @@
 {#if $toast}
 	<div
 		transition:fly={{ duration: 175, easing: bounceInOut, y: '-75px' }}
-		class="fixed bottom-2 left-[50%] z-51 flex min-h-[75px] w-[90%] translate-x-[-50%] flex-col gap-y-2 rounded-lg bg-stone-300 p-2 font-generalSans outline-2 outline-red-600"
+		class="z-51 font-generalSans fixed bottom-2 left-[50%] flex min-h-[75px] w-[90%] translate-x-[-50%] flex-col gap-y-2 rounded-lg bg-stone-300 p-2 outline-2 outline-red-600"
 	>
 		<h1 class="text-lg">{$toast.title}</h1>
 		<span> {$toast.description}</span>
