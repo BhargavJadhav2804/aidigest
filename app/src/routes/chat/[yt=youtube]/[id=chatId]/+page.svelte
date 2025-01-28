8cd16c8f4f105ca974f3b3ca40371fb744d12add
BhargavJ <141632431+BhargavJadhav2804@users.noreply.github.com>
2025-01-28 07:35:52 +0000

    28-01

--- app/src/routes/chat/[yt=youtube]/[id=chatId]/+page.svelte
+++ app/src/routes/chat/[yt=youtube]/[id=chatId]/+page.svelte
@@ -176,8 +176,8 @@
 	</div> -->
 	<div
 		class="z-2 fixed bottom-0 flex w-full justify-between {theme.theme === 'dark'
-			? 'bg-stone-900'
-			: 'bg-stone-800'} sm:outline-hidden gap-x-2 outline-2 outline-stone-600 sm:justify-center"
+			? 'bg-stone-900 sm:bg-transparent'
+			: 'bg-stone-800 sm:bg-transparent'} border-t-1 sm:outline-hidden left-[50%] translate-x-[-50%] gap-x-2 border-stone-500 sm:justify-center sm:border-t-0"
 	>
 		<textarea
 			onkeypress={async (e) => {
@@ -195,10 +195,10 @@
 			name="prompt"
 			rows="3"
 			placeholder="Type something"
-			class="font-generalSans z-2 peer max-h-[10rem] min-h-[5rem] w-[95%] resize-y rounded-none border-r-stone-700 {theme.theme ===
+			class="font-generalSans z-2 peer max-h-[10rem] min-h-[5rem] w-[90%] resize-y rounded-none border-r-stone-600 {theme.theme ===
 			'dark'
 				? 'bg-stone-900'
-				: 'bg-stone-800'} outline-hidden px-3 py-2 text-stone-300 focus:border-r sm:w-[85%] sm:rounded-t-lg sm:border sm:border-b-0 sm:border-x-stone-700 sm:border-t-stone-700"
+				: 'border-r-2 border-r-stone-500 bg-stone-800'} outline-hidden px-3 py-2 text-stone-300 focus:border-r sm:w-[85%] sm:rounded-t-lg sm:border-2 sm:border-b-0 sm:border-x-stone-600 sm:border-t-stone-600"
 			id=""
 		></textarea>
 		<button
@@ -208,7 +208,7 @@
 				generateChat();
 			}}
 			aria-labelledby="Send"
-			class="block size-fit self-center rounded-full bg-stone-900 p-1 outline-1 outline-stone-700 peer-focus:outline sm:hidden"
+			class="mr-1 block size-fit w-[12%] self-center rounded-full bg-stone-900 p-1 outline-1 outline-stone-700 peer-focus:outline sm:hidden"
 		>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -220,14 +220,14 @@
 				stroke-width="2"
 				stroke-linecap="round"
 				stroke-linejoin="round"
-				class="lucide lucide-send mt-1 size-8 rotate-[-44deg] stroke-stone-500 md:size-10"
+				class="lucide lucide-send m-auto mt-1 size-8 rotate-[-44deg] stroke-stone-500 md:size-10"
 				><path
 					d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"
 				/><path d="m21.854 2.147-10.94 10.939" /></svg
 			></button
 		>
 	</div>
-	<div class="mt-[5rem] flex w-[90%] flex-col items-center gap-y-6 md:w-[80%]">
+	<div class="mt-[5rem] flex w-[90%] flex-col items-center gap-y-6 md:w-[85%]">
 		<iframe
 			class="aspect-video h-full w-full rounded-lg"
 			src={`https://www.youtube.com/embed/${data.allChats[0].ytId}`}
@@ -238,7 +238,9 @@
 			allowfullscreen
 		></iframe>
 
-		<div class="font-chillax text-chat summaryChat text-lg">
+		<div
+			class="font-chillax text-chat summaryChat w-full hyphens-auto text-wrap break-words text-lg"
+		>
 			{@html data?.allChats?.[0]?.summary ?? 'Hola'}
 		</div>
 	</div>
